e283c9d8e6978fe6e23112488575887f8ef41337
Charles Guertin <charlesguertin@live.ca>
2022-06-02 21:46:12 -0400

    Fix Home IP, re-enable linkerd

--- dns/variables.tf
+++ dns/variables.tf
@@ -7,7 +7,7 @@ variable "domain_cguertin" {
 variable "router_ip" {
   description = "Router IP address"
   type        = string
-  default     = "173.179.103.20"
+  default     = "24.200.237.46"
 }
 
 variable "domains" {
