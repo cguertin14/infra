7448e186918ee91dd00ac5921878fee8ba539392
Charles Guertin <charles.guertin@lightspeedhq.com>
2024-06-29 14:30:15 -0400

    Update router ip.

--- dns/variables.tf
+++ dns/variables.tf
@@ -13,7 +13,7 @@ variable "account_id" {
 variable "router_ip" {
   description = "Router IP address"
   type        = string
-  default     = "144.172.173.42"
+  default     = "144.172.173.11"
 }
 
 variable "proxied_domains" {
