07268fca6bc5435c846ee98ae039aa3d03d088ff
Charles Guertin <charlesguertin@live.ca>
2024-11-19 13:50:13 -0500

    Update main.tf

--- dns/main.tf
+++ dns/main.tf
@@ -5,6 +5,15 @@ resource "cloudflare_zone" "cguertin_dev" {
   type       = "full"
 }
 
+resource "cloudflare_record" "bsky_validation" {
+  zone_id = cloudflare_zone.cguertin_dev.id
+  type    = "TXT"
+  name    = "_atproto"
+  value   = "did=did:plc:ianrdupaclcx5ojppeap74wh"
+  ttl     = 300
+  proxied = false
+}
+
 resource "cloudflare_record" "pi_load_balancer" {
   zone_id = cloudflare_zone.cguertin_dev.id
   name    = "lb.${var.cguertin_domain}."
