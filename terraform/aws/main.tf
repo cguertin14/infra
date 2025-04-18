d4b81957cfdbfcb4e84f359046422cff9fec75e2
Charles Guertin <charlesguertin@live.ca>
2025-04-18 15:39:02 -0400

    [velero] aws terraform changes

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,3 +1,7 @@
+locals {
+  bucket_prefix = "velero-backups"
+}
+
 data "aws_iam_policy_document" "backup_assume_role_policy" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -9,8 +13,7 @@ data "aws_iam_policy_document" "backup_assume_role_policy" {
 }
 
 resource "aws_iam_user" "backup_user" {
-  for_each = toset(var.role_name_prefixes)
-  name     = "${each.value}-backup-user"
+  name = "${each.value}-backup-user"
 }
 
 # WARNING: Go create this in the console and use copy/paste it from there.
@@ -20,16 +23,14 @@ resource "aws_iam_user" "backup_user" {
 # }
 
 resource "aws_s3_bucket" "backup_bucket" {
-  for_each      = toset(var.role_name_prefixes)
-  bucket_prefix = "${each.value}-backup-bucket"
+  bucket_prefix = "${local.bucket_prefix}-backup-bucket"
   lifecycle {
     prevent_destroy = true # Must be destroyed in the console.
   }
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "expire_old_backups" {
-  for_each = toset(var.role_name_prefixes)
-  bucket   = aws_s3_bucket.backup_bucket[each.key].id
+  bucket = aws_s3_bucket.backup_bucket.id
   rule {
     id     = "expire-old-backups"
     status = "Enabled"
@@ -43,20 +44,22 @@ resource "aws_s3_bucket_lifecycle_configuration" "expire_old_backups" {
 }
 
 resource "aws_s3_bucket_policy" "backup_bucket_policy" {
-  for_each = toset(var.role_name_prefixes)
-  bucket   = aws_s3_bucket.backup_bucket[each.key].id
+  bucket = aws_s3_bucket.backup_bucket.id
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Effect = "Allow"
         Principal = {
-          AWS = aws_iam_user.backup_user[each.key].arn
+          AWS = aws_iam_user.backup_user.arn
         }
         Action = [
-          "s3:PutObject",
           "s3:GetObject",
-          "s3:ListBucket",
+          "s3:DeleteObject",
+          "s3:PutObject",
+          "s3:PutObjectTagging",
+          "s3:AbortMultipartUpload",
+          "s3:ListMultipartUploadParts"
         ]
         Resource = [
           "arn:aws:s3:::${aws_s3_bucket.backup_bucket[each.key].id}",
@@ -68,8 +71,7 @@ resource "aws_s3_bucket_policy" "backup_bucket_policy" {
 }
 
 resource "aws_s3_bucket_public_access_block" "backup_bucket_public_access_block" {
-  for_each = toset(var.role_name_prefixes)
-  bucket   = aws_s3_bucket.backup_bucket[each.key].id
+  bucket = aws_s3_bucket.backup_bucket.id
 
   block_public_acls       = true
   ignore_public_acls      = true
@@ -78,31 +80,43 @@ resource "aws_s3_bucket_public_access_block" "backup_bucket_public_access_block"
 }
 
 resource "aws_s3_bucket_versioning" "backup_bucket_versioning" {
-  for_each = toset(var.role_name_prefixes)
-  bucket   = aws_s3_bucket.backup_bucket[each.key].id
+  bucket = aws_s3_bucket.backup_bucket.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 data "aws_iam_policy_document" "backup_policy" {
-  for_each = toset(var.role_name_prefixes)
   statement {
+    effect = "Allow"
     actions = [
-      "s3:PutObject",
       "s3:GetObject",
-      "s3:ListBucket",
+      "s3:DeleteObject",
+      "s3:PutObject",
+      "s3:PutObjectTagging",
+      "s3:AbortMultipartUpload",
+      "s3:ListMultipartUploadParts"
     ]
     resources = [
-      "arn:aws:s3:::${aws_s3_bucket.backup_bucket[each.key].id}",
-      "arn:aws:s3:::${aws_s3_bucket.backup_bucket[each.key].id}/*",
+      "arn:aws:s3:::${aws_s3_bucket.backup_bucket.id}",
+      "arn:aws:s3:::${aws_s3_bucket.backup_bucket.id}/*",
     ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:DescribeVolumes",
+      "ec2:DescribeSnapshots",
+      "ec2:CreateTags",
+      "ec2:CreateVolume",
+      "ec2:CreateSnapshot",
+      "ec2:DeleteSnapshot"
+    ]
+    resources = ["*"]
   }
 }
 
 resource "aws_iam_user_policy" "backup_user_policy" {
-  for_each = toset(var.role_name_prefixes)
-
   name   = "${each.value}-backup-policy"
   user   = aws_iam_user.backup_user[each.key].id
   policy = data.aws_iam_policy_document.backup_policy[each.key].json
